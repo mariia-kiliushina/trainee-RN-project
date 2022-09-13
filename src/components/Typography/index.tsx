24a7aaa0a450af8e538ecc594e33ee47bbe35f0e
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-09-13 11:25:24 +0400

    feat: add fonts and typography, add ANDROID_HOME to scripts

---
+++ src/components/Typography/index.tsx
@@ -0,0 +1,35 @@
+import React, {ReactNode} from 'react';
+import {StyleSheet, View, Text, TextProps} from 'react-native';
+import {FC} from 'react';
+import {capitalizeFirstLetter} from '../helpers';
+
+type FontType = 'regular' | 'bold';
+
+type Props = TextProps & {
+  fontType: FontType;
+  children: ReactNode;
+};
+
+const Typography: FC<Props> = ({
+  children,
+  fontType = 'regular',
+  ...restProps
+}) => {
+  const fontFamily = 'Inter-' + capitalizeFirstLetter(fontType);
+
+  return (
+    <View>
+      <Text style={[styles.text, {fontFamily: fontFamily}]} {...restProps}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 34,
+  },
+});
+
+export default Typography;
