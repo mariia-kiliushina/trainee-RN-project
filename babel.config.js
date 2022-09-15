25ae19d8bbc345e4dd1240bd9cf15a41231df549
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-09-15 11:27:58 +0400

    feat:remove relative imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          components: './src/components',
+          constants: './src/constants',
+          assets: './src/assets',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
