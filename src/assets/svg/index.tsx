0357ca57c7cfc19127545b8ce7ec09b9135ebb90
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-09-16 11:56:50 +0400

    feat: add ts watcher

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -7,7 +7,7 @@ export {default as PlusSquared} from './plus-squared.svg';
 export {default as Pen} from './pen.svg';
 export {default as Cross} from './cross.svg';
 export {default as Arrow} from './arrow.svg';
-export {default as User} from './user.svg';
+export {default as UserIcon} from './user.svg';
 export {default as HomeIcon} from './home.svg';
 export {default as BudgetIcon} from './pie-chart.svg';
 export {default as Settings} from './settings.svg';
