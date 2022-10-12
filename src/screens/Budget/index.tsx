56f6e8194f896b342776972b866c96866609456a
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-10-12 15:28:56 +0400

    remove and clean screens

--- src/screens/Budget/index.tsx
+++ src/screens/Budget/index.tsx
@@ -2,17 +2,11 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {COLORS} from 'constants/colors';
 import {Container} from 'src/components/Container';
-import {Slider} from 'components/Slider';
-import {Button} from 'components/Button';
 
 export const Budget = () => {
   return (
     <Container style={styles.main}>
-      <Slider style={styles.customizedHorizontalSlider} />
-      <View style={styles.content}>
-        <Button type="primary">Click</Button>
-        <Button type="secondary">Click</Button>
-      </View>
+      <View>Budget</View>
     </Container>
   );
 };
@@ -22,12 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.baseLight80,
     paddingHorizontal: 0,
-  },
-  customizedHorizontalSlider: {
-    paddingTop: 30,
-  },
-  content: {
-    paddingHorizontal: 20,
-    paddingTop: '15%',
   },
 });
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,6 @@
-import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Typography} from 'components/Typography';
+import React from 'react';
+import {StyleSheet, View, Pressable} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Button} from 'components/Button';
 import {RootStackParamList} from 'src/navigation/stack';
 import {Container} from 'src/components/Container';
 import {COLORS} from 'constants/colors';
@@ -10,70 +8,13 @@ import {COLORS} from 'constants/colors';
 export const Home = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList>) => {
-  const [timesPressedPrimary, setTimesPressedPrimary] = useState(0);
-  const [timesPressedSecondary, setTimesPressedSecondary] = useState(0);
-  const [timesPressedLink, setTimesPressedLink] = useState(0);
-
   const onNavigate = () => {
     navigation.navigate('AddRecord');
   };
-  const firstGroupPressHanler = () =>
-    setTimesPressedPrimary(current => current + 1);
-
-  const secondGroupPressHandler = () =>
-    setTimesPressedSecondary(current => current + 1);
-
-  const linkPressHandler = () => setTimesPressedLink(current => current + 1);
-
   return (
     <Container style={styles.main}>
-      <Typography variant="18" fontType="bold">
-        {timesPressedPrimary}
-      </Typography>
-      <View style={styles.buttonsContainer}>
-        <Button
-          onPress={firstGroupPressHanler}
-          iconName="plusSquared"
-          type="primary"
-          style={styles.buttonLeft}>
-          Press me
-        </Button>
-        <Button
-          onPress={firstGroupPressHanler}
-          type="primary"
-          style={styles.buttonRight}>
-          Press me
-        </Button>
-      </View>
-      <Typography variant="18" fontType="bold">
-        {timesPressedSecondary}
-      </Typography>
-      <Button onPress={secondGroupPressHandler} iconName="pen" type="primary">
-        Press me
-      </Button>
-      <Button onPress={secondGroupPressHandler} type="primary">
-        Press me
-      </Button>
-      <Button onPress={secondGroupPressHandler} iconName="pen" type="secondary">
-        Press me
-      </Button>
-      <Button onPress={secondGroupPressHandler} type="secondary">
-        Press me
-      </Button>
-      <Typography variant="18" fontType="bold">
-        {timesPressedLink}
-      </Typography>
-
-      <Button onPress={linkPressHandler} type="link">
-        Press
-      </Button>
-      <Button
-        onPress={onNavigate}
-        iconStyle={styles.iconStyle}
-        style={styles.navigationButton}
-        type="secondary">
-        Go to another screen
-      </Button>
+      <View>Home</View>
+      <Pressable onPress={onNavigate}>Another screen</Pressable>
     </Container>
   );
 };
@@ -82,24 +23,5 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     backgroundColor: COLORS.baseLight80,
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  buttonLeft: {
-    flex: 1,
-  },
-  buttonRight: {
-    flex: 1,
-    marginLeft: 16,
-  },
-  navigationButton: {
-    marginTop: 'auto',
-    width: '50%',
-    alignSelf: 'flex-end',
-  },
-  iconStyle: {
-    marginRight: 0,
   },
 });
