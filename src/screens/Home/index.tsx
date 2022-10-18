e2a467d550dd2930cc3cad61a3b85a7e1b90387b
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-10-18 13:36:49 +0400

    FAT-44381/input add numeric

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,13 +3,16 @@ import {StyleSheet, Pressable, View} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from 'src/navigation/stack';
 import {Container} from 'src/components/Container';
-import {InputAccount} from 'src/components/InputAccount';
+import {Input} from 'src/components/Input';
 import {InputPassword} from 'src/components/InputPassword';
+import {InputNumeric} from 'src/components/InputNumeric';
 import {Typography} from 'src/components/Typography';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const ACCOUNT = 'Account Number';
 const PASSWORD = 'Password';
+const INPUT = 'Input';
+
 export const Home = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList>) => {
@@ -20,13 +23,14 @@ export const Home = ({
     <Container style={styles.main}>
       <KeyboardAwareScrollView>
         <View>
-          <InputAccount label={ACCOUNT} />
-          <InputAccount label={ACCOUNT} type="disabled" />
-          <InputAccount label={ACCOUNT} type="error" />
-          <InputAccount
-            label={ACCOUNT}
+          <Input label={INPUT} placeholder="Input" />
+          <Input label={INPUT} type="disabled" placeholder="Input" />
+          <Input label={INPUT} type="error" placeholder="Input" />
+          <Input
+            label={INPUT}
             type="error"
-            errorText="Account number does not exist"
+            errorText="Value does not exist"
+            placeholder="Input"
           />
         </View>
         <View>
@@ -37,6 +41,28 @@ export const Home = ({
             label={PASSWORD}
             type="error"
             errorText="Password is incorrect"
+          />
+        </View>
+        <View>
+          <InputNumeric
+            label={ACCOUNT}
+            placeholder="Enter your account number"
+          />
+          <InputNumeric
+            label={ACCOUNT}
+            type="disabled"
+            placeholder="Enter your account number"
+          />
+          <InputNumeric
+            label={ACCOUNT}
+            type="error"
+            placeholder="Enter your account number"
+          />
+          <InputNumeric
+            label={PASSWORD}
+            type="error"
+            errorText="Account number is incorrect"
+            placeholder="Enter your account number"
           />
         </View>
         <Pressable onPress={onNavigate} style={styles.button}>
