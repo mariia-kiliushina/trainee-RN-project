2bac24b319c4d795cafcd45e776cfdc6f7c1d0db
Mariia Kiliushina <mariia.kiliushina@gmail.com>
2022-12-09 14:46:40 +0400

    FAT-47449 fix typing

--- src/screens/Posts/index.tsx
+++ src/screens/Posts/index.tsx
@@ -94,7 +94,7 @@ export const Posts = ({navigation}: RootStackScreenProps<'Posts'>) => {
     });
   };
 
-  const renderItem = (data: {item: TPost}) => (
+  const renderItem = (data: {item: TPostKey}) => (
     <Pressable style={styles.rowVisible}>
       <Typography variant="18" fontType="bold">
         {data.item.title}
@@ -103,7 +103,7 @@ export const Posts = ({navigation}: RootStackScreenProps<'Posts'>) => {
     </Pressable>
   );
 
-  const renderHiddenItem = (data: {item: TPost}, rowMap: any) => (
+  const renderHiddenItem = (data: {item: TPostKey}, rowMap: any) => (
     <View style={styles.rowHidden}>
       <Pressable
         onPress={() => onDelete({rowMap, rowKey: data.item.key})}
@@ -165,135 +165,3 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
 });
-
-// import {useState} from 'react';
-// import {
-//   StyleSheet,
-//   Text,
-//   TouchableOpacity,
-//   TouchableHighlight,
-//   View,
-// } from 'react-native';
-
-// import {SwipeListView} from 'react-native-swipe-list-view';
-
-// export function Posts() {
-//   const [listData, setListData] = useState(
-//     Array(20)
-//       .fill('')
-//       .map((_, i) => ({key: `${i}`, text: `item #${i}`})),
-//   );
-
-//   const closeRow = (rowMap, rowKey) => {
-//     console.log('rowMap');
-//     console.log(rowMap);
-//     console.log('rowKey');
-//     console.log(rowKey);
-//     console.log('rowMap[rowKey]');
-//     console.log(rowMap[rowKey]);
-
-//     if (rowMap[rowKey]) {
-//       rowMap[rowKey].closeRow();
-//     }
-//   };
-
-//   const deleteRow = (rowMap, rowKey) => {
-//     closeRow(rowMap, rowKey);
-//     const newData = [...listData];
-//     const prevIndex = listData.findIndex(item => item.key === rowKey);
-//     newData.splice(prevIndex, 1);
-//     setListData(newData);
-//   };
-
-//   const onRowDidOpen = rowKey => {
-//     console.log('This row opened', rowKey);
-//   };
-
-//   const renderItem = data => (
-//     <TouchableHighlight
-//       onPress={() => console.log('You touched me')}
-//       style={styles.rowFront}
-//       underlayColor={'#AAA'}
-//     >
-//       <View>
-//         <Text>I am {data.item.text} in a SwipeListView</Text>
-//       </View>
-//     </TouchableHighlight>
-//   );
-
-//   const renderHiddenItem = (data, rowMap) => (
-//     <View style={styles.rowBack}>
-//       <Text>Left</Text>
-//       <TouchableOpacity
-//         style={[styles.backRightBtn, styles.backRightBtnLeft]}
-//         onPress={() => closeRow(rowMap, data.item.key)}
-//       >
-//         <Text style={styles.backTextWhite}>Close</Text>
-//       </TouchableOpacity>
-//       <TouchableOpacity
-//         style={[styles.backRightBtn, styles.backRightBtnRight]}
-//         onPress={() => deleteRow(rowMap, data.item.key)}
-//       >
-//         <Text style={styles.backTextWhite}>Delete</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-
-//   return (
-//     <View style={styles.container}>
-//       <SwipeListView
-//         data={listData}
-//         renderItem={renderItem}
-//         renderHiddenItem={renderHiddenItem}
-//         leftOpenValue={75}
-//         rightOpenValue={-150}
-//         previewRowKey={'0'}
-//         previewOpenValue={-40}
-//         previewOpenDelay={3000}
-//         onRowDidOpen={onRowDidOpen}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: 'white',
-//     flex: 1,
-//   },
-//   backTextWhite: {
-//     color: '#FFF',
-//   },
-//   rowFront: {
-//     alignItems: 'center',
-//     backgroundColor: '#CCC',
-//     borderBottomColor: 'black',
-//     borderBottomWidth: 1,
-//     justifyContent: 'center',
-//     height: 50,
-//   },
-//   rowBack: {
-//     alignItems: 'center',
-//     backgroundColor: '#DDD',
-//     flex: 1,
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     paddingLeft: 15,
-//   },
-//   backRightBtn: {
-//     alignItems: 'center',
-//     bottom: 0,
-//     justifyContent: 'center',
-//     position: 'absolute',
-//     top: 0,
-//     width: 75,
-//   },
-//   backRightBtnLeft: {
-//     backgroundColor: 'blue',
-//     right: 75,
-//   },
-//   backRightBtnRight: {
-//     backgroundColor: 'red',
-//     right: 0,
-//   },
-// });
